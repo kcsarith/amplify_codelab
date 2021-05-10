45b76625bb7a8b8fa073c08fefab62ec86e2187e
kcsarith <krisnasarith@gmail.com>
2021-05-10 01:43:25 -0700

    update amplify

--- lib/amplifyconfiguration.dart
+++ lib/amplifyconfiguration.dart
@@ -1,19 +1,6 @@
 const amplifyconfig = ''' {
     "UserAgent": "aws-amplify-cli/2.0",
     "Version": "1.0",
-    "analytics": {
-        "plugins": {
-            "awsPinpointAnalyticsPlugin": {
-                "pinpointAnalytics": {
-                    "appId": "c38a60ffc6a84594ab10f5ffd7220171",
-                    "region": "us-west-2"
-                },
-                "pinpointTargeting": {
-                    "region": "us-west-2"
-                }
-            }
-        }
-    },
     "auth": {
         "plugins": {
             "awsCognitoAuthPlugin": {
@@ -25,25 +12,21 @@ const amplifyconfig = ''' {
                 "CredentialsProvider": {
                     "CognitoIdentity": {
                         "Default": {
-                            "PoolId": "us-west-2:ea300c62-4b98-4ff3-945d-05a40dd0b6f7",
+                            "PoolId": "us-west-2:bd7b260c-b623-4786-81f7-7b8731a15d3b",
                             "Region": "us-west-2"
                         }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-west-2_OS8dRbveY",
+                        "AppClientId": "3r0k91o06k0f2itgbkqhhc49g0",
+                        "Region": "us-west-2"
                     }
                 },
                 "Auth": {
                     "Default": {
                         "authenticationFlowType": "USER_SRP_AUTH"
-                    }
-                },
-                "PinpointAnalytics": {
-                    "Default": {
-                        "AppId": "c38a60ffc6a84594ab10f5ffd7220171",
-                        "Region": "us-west-2"
-                    }
-                },
-                "PinpointTargeting": {
-                    "Default": {
-                        "Region": "us-west-2"
                     }
                 }
             }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify.dart';
-import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'amplifyconfiguration.dart';
 import 'modules/home/screens/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await _configureAmplify();
   runApp(MyApp());
+}
+
+Future<void> _configureAmplify() async {
+  // if (!mounted) return;
+
+  // Add Pinpoint and Cognito Plugins
+  AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
+  Amplify.addPlugins([authPlugin]);
+
+  // Once Plugins are added, configure Amplify
+  // Note: Amplify can only be configured once.
+  try {
+    print('Configuring amplify...');
+    await Amplify.configure(amplifyconfig);
+    // amplifyConfigured = true;
+    print('Finished configuring amplify');
+  } on AmplifyAlreadyConfiguredException {
+    print("Amplify was already configured. Was the app restarted?");
+  } catch (e) {
+    print(e);
+  }
 }
 
 class MyApp extends StatefulWidget {
@@ -16,30 +38,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool amplifyConfigured = false;
-  @override
-  void initState() {
-    super.initState();
-    _configureAmplify();
-  }
-
-  void _configureAmplify() async {
-    if (!mounted) return;
-
-    // Add Pinpoint and Cognito Plugins
-    Amplify.addPlugin(AmplifyAnalyticsPinpoint());
-    Amplify.addPlugin(AmplifyAuthCognito());
-
-    amplifyConfigured = true;
-    // Once Plugins are added, configure Amplify
-    // Note: Amplify can only be configured once.
-    try {
-      await Amplify.configure(amplifyconfig);
-    } on AmplifyAlreadyConfiguredException {
-      print("Amplify was already configured. Was the app restarted?");
-    } catch (e) {
-      print(e);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
