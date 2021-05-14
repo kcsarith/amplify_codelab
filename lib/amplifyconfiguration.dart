ed0389ceb80338465135a30fb2a31724211161b7
kcsarith <krisnasarith@gmail.com>
2021-05-14 13:24:44 -0700

    update amplify for GraphQL API

--- lib/amplifyconfiguration.dart
+++ lib/amplifyconfiguration.dart
@@ -12,15 +12,15 @@ const amplifyconfig = ''' {
                 "CredentialsProvider": {
                     "CognitoIdentity": {
                         "Default": {
-                            "PoolId": "us-west-2:bd7b260c-b623-4786-81f7-7b8731a15d3b",
+                            "PoolId": "us-west-2:bc9348fa-e6e9-4cba-9188-36f4ff01d2ec",
                             "Region": "us-west-2"
                         }
                     }
                 },
                 "CognitoUserPool": {
                     "Default": {
-                        "PoolId": "us-west-2_OS8dRbveY",
-                        "AppClientId": "3r0k91o06k0f2itgbkqhhc49g0",
+                        "PoolId": "us-west-2_KmPnX4Oqj",
+                        "AppClientId": "10valaj8ltp45dibj72n4pu4ne",
                         "Region": "us-west-2"
                     }
                 },
@@ -28,7 +28,35 @@ const amplifyconfig = ''' {
                     "Default": {
                         "authenticationFlowType": "USER_SRP_AUTH"
                     }
+                },
+                "AppSync": {
+                    "Default": {
+                        "ApiUrl": "https://h5cgaod7qjdclnakzfhaceh46y.appsync-api.us-west-2.amazonaws.com/graphql",
+                        "Region": "us-west-2",
+                        "AuthMode": "API_KEY",
+                        "ApiKey": "da2-c5xtuodsjvgptin3lo7tuiotf4",
+                        "ClientDatabasePrefix": "amplifycodelab_API_KEY"
+                    },
+                    "amplifycodelab_AWS_IAM": {
+                        "ApiUrl": "https://h5cgaod7qjdclnakzfhaceh46y.appsync-api.us-west-2.amazonaws.com/graphql",
+                        "Region": "us-west-2",
+                        "AuthMode": "AWS_IAM",
+                        "ClientDatabasePrefix": "amplifycodelab_AWS_IAM"
+                    }
                 }
+            }
+        }
+    },
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "amplifycodelab": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://h5cgaod7qjdclnakzfhaceh46y.appsync-api.us-west-2.amazonaws.com/graphql",
+                    "region": "us-west-2",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-c5xtuodsjvgptin3lo7tuiotf4"
+                },
             }
         }
     }
